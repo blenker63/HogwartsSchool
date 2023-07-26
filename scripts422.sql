ddc43f093f820de6c1deac5238aead710db6209e
vaceslavkameronov <slka@mail.ru>
2023-07-26 21:30:32 +0400

    доработка ДЗ№4.2

--- scripts422.sql
+++ scripts422.sql
@@ -3,18 +3,18 @@ CREATE TABLE person
     "name"  TEXT PRIMARY KEY UNIQUE   NOT NULL,
     age     INTEGER CHECK ( age > 0 ) NOT NULL,
     license BOOLEAN                   NOT NULL,
-    idCar   INTEGER
+    idCar   INTEGER REFERENCES car (id)
 );
 
 CREATE TABLE car
 (
-    idCar   INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    id   INTEGER PRIMARY KEY UNIQUE NOT NULL,
     "brand" TEXT,
     "model" TEXT,
     price   INTEGER
 );
 
 SELECT person.name, person.age, person.license,
-    brand, model, price
+       car.brand, model, price
 FROM person
-LEFT JOIN car c on person.idCar = c.idCar;
+         LEFT JOIN car  on person.idCar = car.id;
